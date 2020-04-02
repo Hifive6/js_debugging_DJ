49bb8a2e91e21ef1a2dca365c40267adb05f5f82
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-04-02 13:22:00 -0400

    initial file

---
+++ main.js
@@ -0,0 +1,53 @@
+var woolOwners = [
+    {
+        "master": 1
+    },
+    {
+        "dame": 1
+    },
+    {
+      "little boy": 1,
+      "location": "down the lane"
+    }
+  ];
+  
+  
+  
+  
+  var haveYouAnyWool = function() {
+      for (var i = 0; i < woolOwners.length; i++) {
+    //   var totalBags = totalBags + i;
+    //   console.log(totalBags);
+      }
+      return (i);
+  };
+  
+  var bags = haveYouAnyWool();
+
+  function baabaaBlackSheep() {
+      console.log("BaaBaa BlackSheep have you any wool?");
+    //   var bag = bags;
+    //   console.log(bag);
+      if (bags > 0) {
+          console.log("yes sir, yes sir " + bags + " bags full");
+    }
+  }
+  
+  function oneForMy() {
+      for (var i = 0; i < 2; i++) {
+          people = Object.keys(woolOwners[i]);
+          var person = people.toString();
+          console.log("one for my " + person);
+      }
+  }
+  
+//   haveYouAnyWool();
+  baabaaBlackSheep();
+  oneForMy();
+  
+  var boy = Object.keys(woolOwners[2]);
+//   console.log(boy);
+  var littleBoy = boy[0];
+  
+  var whereHeLives = woolOwners[2].location;
+  console.log("one for the " + littleBoy + " that lives " + whereHeLives);
